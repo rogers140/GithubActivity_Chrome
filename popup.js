8cc913280f75e242d10bcb670a389f368e3fd201
rogers140 <rogers140@gmail.com>
2017-01-26 23:22:02 -0800

    Add git contribution graph.

--- popup.js
+++ popup.js
@@ -1,12 +1,40 @@
-function isLoggedIn() {
-	return false;
+function setUsername(username) {
+	chrome.storage.local.set({'gitUsername': username}, function() {
+    alert('Username ' + username +' Saved!');
+  });
 }
+
+function showContributionGraph(username) {
+	//Show content
+	console.log(username);
+	document.getElementById("content").innerHTML+="<img src='http://ghchart.rshah.org/"+
+		username + "' alt='" + username + "' />" +
+		"<button id='change_account'>Change Account</button>";
+}
+
+function setAndShow() {
+	var new_username = document.getElementById('username_input').value;
+	setUsername(new_username);
+	showContributionGraph(new_username);
+}
+
+//Main
 document.addEventListener('DOMContentLoaded', function () {
-	if (isLoggedIn()) {
-		//Show content
-	} else {
-		document.getElementById("content").innerHTML+="<span>Username: </span><input/>" + "<br/>" +
-		"<span>Password: </span><input type='password'/>" + "<br/>" +
-		"<button>Submit</button>";
-	}
+	var username = "";
+	// Read local storages for username
+	chrome.storage.local.get('gitUsername', function(result) {
+		try {
+			username = result.gitUsername;
+		} catch(e) {
+			username = "";
+		}
+		console.log(username);
+		if (username != "") {
+			showContributionGraph(username);
+		} else {
+			document.getElementById("content").innerHTML+="<span>Username: </span><input id='username_input'/>" + "<br/>" +
+			"<button id='submit'>Submit</button>";
+			document.getElementById("submit").addEventListener("click", setAndShow);
+		}
+	});
 });
